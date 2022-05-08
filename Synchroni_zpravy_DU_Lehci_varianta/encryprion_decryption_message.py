468de6fd4ba33eae69ffb9ff5fc3d5e1b6abbad2
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2022-05-08 15:46:23 +0200

    Add files via upload

--- Synchroni_zpravy_DU_Lehci_varianta/encryprion_decryption_message.py
+++ Synchroni_zpravy_DU_Lehci_varianta/encryprion_decryption_message.py
@@ -1,16 +1,50 @@
-from cryptography.fernet import Fernet
+from cryptography import Fernet
+from rsa import encrypt
 
-zprava = "Survive..."
+#Třída na zašifrování souboru
+#
 
-key = Fernet.generate_key()
+class Encryptor():
 
-fernet = Fernet(key)
+    def key_create(self):
+        key = Fernet.generate_key()
+        return key
+    
+    def key_write(self, key, key_name):
+        with open(key_name, 'wb') as mykey:
+            mykey.write(key)
+        
+    def key_load(self, key_name, key):
+        with open(key_name, 'rb') as mykey:
+            mykey.read()
+        return key
 
-endzpravy = fernet.encrypt(zprava.encode())
+#
+#   Šifrování souboru pomocí synchroního šifrování
+#  
 
-print("originální zpráva: ", zprava)
-print("Zašifrovaný zpráva: ", endzpravy)
 
-des_zpravy = fernet.decrypt(endzpravy).decode()
+    def file_encrypt(self, key, original_file, encrypted_file):
 
-print("dešifrovaný zpráva: ", des_zpravy)
+        f = Fernet(key)
+
+        with open('test.txt', 'rb') as original_file:
+            original = original_file.read()
+
+
+        encrypted = f.encrypt(original)
+
+        with open (encrypted_file, 'wb') as original_file:
+            original_file.write(encrypted)
+        
+#Dešifrování souboru
+# 
+
+    def file_decrypt(self, key, original_file, decrypted_file):
+
+        f= Fernet(key)
+
+        with open('test.txt', 'rb') as original_file:
+            encrypted = original_file.read()
+
+        decrypted = f.decrypt(encrypted)    
